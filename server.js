b100f5c8f653b05be94724a73781b3ef0d521d3b
pinsKnots <manishrkumar8991@gmail.com>
2024-02-12 16:11:23 +0530

    mern first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express()
+import { join } from 'path'
+import {logger} from './middleware/logger'
+
+const PORT = process.env.PORT || 3500
+
+app.use(logger)
+app.use('/',join(__dirname,'/public'))
+app.use('/',require('./routes/root'))
+app.all('*',(req,res) => {
+     res.status(404)
+     if(req.accepts('Html')){
+        res.sendFile(join(__dirname,'views','404.html'))
+     }else if(req.accepts('JSON')){
+        res.json({message : 'page not found'})
+     }else{
+        res.type('text').send("404 error")
+     }
+})
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
